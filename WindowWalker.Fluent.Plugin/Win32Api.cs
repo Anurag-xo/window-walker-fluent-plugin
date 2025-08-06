9cafa665e603d97fffeea07ae5399be32f2eabe2
Anurag-xo <amnrag889@gmail.com>
2025-08-06 08:52:24 +0000

    feat: Add Logging, Handle Exceptions, Fix Text Length

--- WindowWalker.Fluent.Plugin/Win32Api.cs
+++ WindowWalker.Fluent.Plugin/Win32Api.cs
@@ -1,24 +1,23 @@
 using System;
 using System.Runtime.InteropServices;
-
 namespace WindowWalker.Fluent.Plugin
 {
     internal static class Win32Api
     {
         [DllImport("user32.dll")]
         public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
-
         [DllImport("user32.dll")]
         public static extern bool IsWindowVisible(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
+        // Updated signature with SetLastError and CharSet
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public static extern int GetWindowText(IntPtr hWnd, char[] lpString, int nMaxCount);
-
         [DllImport("user32.dll")]
         public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
-
         [DllImport("user32.dll")]
         public static extern bool SetForegroundWindow(IntPtr hWnd);
+        // Add GetWindowTextLength
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
 
         public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
     }
